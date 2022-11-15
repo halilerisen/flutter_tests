cd468a80336c4a9ea91799ec6818d562407d8d52
h <H>
2022-11-15 15:06:30 +0300

    add Unit Test Group

--- unit_test/a1_unit_test/lib/maths_util.dart
+++ unit_test/a1_unit_test/lib/maths_util.dart
@@ -1,3 +1,11 @@
 int sum({required int a, required int b}) {
   return a + b;
 }
+
+int subtract({required int a, required int b}) {
+  return a - b;
+}
+
+int multiply({required int a, required int b}) {
+  return a * b;
+}
--- unit_test/a1_unit_test/test/maths_util_test.dart
+++ unit_test/a1_unit_test/test/maths_util_test.dart
@@ -2,15 +2,36 @@ import 'package:a1_unit_test/maths_util.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  test('Test sum function', () {
-    // Arrange
-    const int a = 10;
-    const int b = 5;
+  group(
+    'Maths Util - ',
+    () {
+      // Arrange
+      const int a = 10;
+      const int b = 5;
 
-    // Act
-    final sumResult = sum(a: a, b: b);
+      test('Test sum function', () {
+        // Act
+        final sumResult = sum(a: a, b: b);
 
-    // Assert
-    expect(sumResult, 15);
-  });
+        // Assert
+        expect(sumResult, 15);
+      });
+
+      test('Test subtract function', () {
+        // Act
+        final sumResult = subtract(a: a, b: b);
+
+        // Assert
+        expect(sumResult, 5);
+      });
+
+      test('Test multiply function', () {
+        // Act
+        final sumResult = multiply(a: a, b: b);
+
+        // Assert
+        expect(sumResult, 50);
+      });
+    },
+  );
 }
